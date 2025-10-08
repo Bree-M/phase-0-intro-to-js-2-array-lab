468ccd09b8fe708341a6652237de8398853b12c6
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-10-08 04:16:03 +0300

    arrays

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-  const cats = ["Milo","Otis","Garfield"];
-  
-  
-  function destructivelyAppendCat(name) {
-    cats.push(name);
-  }
-  
-  
-  function destructivelyPrependCat(name) {
-    cats.unshift(name);
-  }
-  
-  
-  function destructivelyRemoveLastCat() {
-    cats.pop();
-  }
-  
-  
-  function destructivelyRemoveFirstCat() {
-    cats.shift();
-  }
-  
-  
-  function appendCat(name) {
-    return [...cats, name];  
-  }
-  
+const cats=[
+  "Milo",
+  "Otis",
+  "Garfield",
+];
 
-  function prependCat(name) {
-    return [name, ...cats];
-  }
+function destructivelyAppendCat(name){
+  cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+  cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.shift()
+}
+
+function appendCat(name){
+  return [...cats,name]
+}
+// append means adding to the end and prepend means to add at the beginning.
+
+
+function prependCat(name){
+  return [name,...cats]
+}
+
+
+function removeLastCat(){
+  return cats.slice(0,-1);
   
-  
-  function removeLastCat() {
-    return cats.slice(0, -1);  
-  }
-  
-  
-  function removeFirstCat() {
-    return cats.slice(1);  
-  }
-  
+}
+
+function removeFirstCat(){
+  return cats.slice(1)
+}
